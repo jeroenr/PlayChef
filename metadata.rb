12dfeeaab83e2d63c473760aaec179e7024c488f
Sameer Arora <sarora@apache.org>
2014-02-28 16:34:13 +0530

    Added Dependency for Java

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,12 @@ license          'Apache 2.0'
 description      'Deploys Play Application'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+depends 'java'
+
+supports 'debian'
+supports 'ubuntu'
+supports 'centos'
+supports 'redhat'
+
+recipe 'deploy-play::default', 'Deploys Play Application on your Node'
