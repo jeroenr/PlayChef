0f4fab4c3a4861716b3942a4329d5ae1d584deb6
Jeroen Rosenberg <jeroen.rosenberg@ibanx-hse.com>
2014-06-13 09:25:23 +0200

    Sensible defaults

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,9 +1,9 @@
-default['play_app']['install_dir'] = "/usr/src"
+default['play_app']['install_dir'] = "/opt/play-app"
 default['play_app']['dist_url'] = "http://downloads.typesafe.com/play/"
 default['play_app']['application_name'] = 'play-app'
-default['play_app']['config_dir'] = '/opt/configuration'
+default['play_app']['config_dir'] = '/etc/play-app'
 default['play_app']['vm_options']='-Xms512M -Xmx1024M -Xss1M -XX:MaxPermSize=512M'
-default['play_app']['pid_file_path']="/var/run/play.pid"
+default['play_app']['pid_file_path']="/var/run/play-app.pid"
 default['play_app']['application_secret_key']="Your Secret Key here"
 default['play_app']['play_log_level']="DEBUG"  
 default['play_app']['app_log_level']="DEBUG"
